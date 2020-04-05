f02eaf185fd2fa42ec11d54650e92ffa5e675020
uusa35 <uusa35@gmail.com>
2020-04-05 22:19:25 +0300

    updates

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Traits\ImageHelpers;
+use App\Services\Traits\OrderTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,7 +11,7 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ImageHelpers, OrderTrait;
     const TAKE = 100;
     const TAKE_MID = 24;
     const TAKE_MIN = 12;
