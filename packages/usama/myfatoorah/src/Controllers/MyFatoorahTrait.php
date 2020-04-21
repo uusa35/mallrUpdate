42793e9d8009bff1a8892fe746cb973d9e4cd35b
uusa35 <uusa35@gmail.com>
2020-04-21 18:09:43 +0300

    updates

--- packages/usama/myfatoorah/src/Controllers/MyFatoorahTrait.php
+++ packages/usama/myfatoorah/src/Controllers/MyFatoorahTrait.php
@@ -20,9 +20,9 @@ trait MyFatoorahTrait
             curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array('grant_type' => 'password', 'username' => ENV('MYFATOORAH_USERNAME'), 'password' => ENV('MYFATOORAH_PASSWORD'))));
             $result = curl_exec($curl);
             $info = curl_getinfo($curl);
-            dd($info);
             curl_close($curl);
             $json = json_decode($result, true);
+            dd($json);
             if (isset($json['access_token']) && !empty($json['access_token'])) {
                 $access_token = $json['access_token'];
             } else {
