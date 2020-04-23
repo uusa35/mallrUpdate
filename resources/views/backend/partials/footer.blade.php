419f2515cc479288bdb80ef4525faaf339bee48f
uusa35 <uusa35@gmail.com>
2020-04-23 08:56:14 +0300

    updates

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- BEGIN FOOTER -->
 <div class="page-footer">
     <div class="page-footer-inner"> {{ Carbon\Carbon::today()->format('Y') }} &copy;  &#32;
-        <a href="http://ideasowners.net" title="" target="_blank" style="color : darkgoldenrod">{{ env('APP_CASE') }} co. &#32;</a>
+        <a href="{{ env('APP_URL') }}" title="" target="_blank" style="color : darkgoldenrod">{{ env('APP_CASE') }} co. &#32;</a>
     </div>
     <div class="scroll-to-top">
         <i class="icon-arrow-up"></i>
