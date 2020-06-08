31873b4eb644591a58768bcf88ef42251578f346
uusa35 <uusa35@gmail.com>
2020-06-08 14:43:39 +0300

    updates

--- resources/views/backend/partials/_sidebar_admin.blade.php
+++ resources/views/backend/partials/_sidebar_admin.blade.php
@@ -487,7 +487,7 @@
 
 @can('index','order')
     <li class="nav-item {{ activeItem('order') }}">
-        <a href="{{ route('backend.admin.order.index') }}" class="nav-link nav-toggle">
+        <a href="{{ route('backend.admin.order.index',['paid' => true]) }}" class="nav-link nav-toggle">
             <i class="fa fa-fw fa-money"></i>
             <span class="title">{{ trans('general.orders') }}</span>
             <span class="arrow"></span>
