d809aa990f5363ee3e958b27440a3e5f97579f21
uusa35 <uusa35@gmail.com>
2020-06-17 23:13:05 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/styles.blade.php
+++ resources/views/frontend/wokiee/four/partials/styles.blade.php
@@ -82,7 +82,7 @@
 
     }
 
-    @elseif(env('DAILY'))
+    @elseif(env('DAILY') || env('HTB'))
     body, html, a, p, h1, h2, h3, h4, h5, h6, table, row, td, th, tr, td, thead, tfoot, span, .btn, ul, li,
     .container, .tt-title-small, .tt-description, header, footer
     .tt-collapse-title, .tt-collapse-content, .tt-collapse-block .tt-item .tt-collapse-title,
