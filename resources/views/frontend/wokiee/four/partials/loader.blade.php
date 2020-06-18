6f85fa5d25b481b1f633566ef5a1d957becd69fc
uusa35 <uusa35@gmail.com>
2020-06-18 17:45:22 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/loader.blade.php
+++ resources/views/frontend/wokiee/four/partials/loader.blade.php
@@ -1,5 +1,5 @@
 <div id="loader-wrapper" style="display: flex; flex-direction: column; justify-content: center; align-items: center; flex-wrap: nowrap; background-color: {{ $settings->main_theme_bg_color }}">
-    <div><img class="img-responsive img-sm" src="{{ $settings->getCurrentImageAttribute('logo') }}" alt="{{ $settings->company }}"></div><br>
+    <div><img class="img-responsive img-sm" src="{{ $settings->imageThumbLink }}" alt="{{ $settings->company }}"></div><br>
     <div>{{ trans("general.loading") }}</div>
     <div id="loader" style="margin-top: 100px;">
         <div class="dot"></div>
