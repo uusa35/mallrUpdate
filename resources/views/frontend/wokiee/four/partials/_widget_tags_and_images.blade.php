bf2df19177a2e4a6dbc98750570995d47066bbbe
uusa35 <uusa35@gmail.com>
2020-06-25 00:58:04 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_widget_tags_and_images.blade.php
+++ resources/views/frontend/wokiee/four/partials/_widget_tags_and_images.blade.php
@@ -6,7 +6,7 @@
         <img class="" src="{{ $element->images->first()->imageLargeLink }}" alt="{{ $element->description }}">
 </span>
 @endif
-<span class="tt-label-location d-none d-md-block">
+{{--<span class="tt-label-location d-none d-md-block">--}}
 {{--    @if($element->on_new)--}}
 {{--        <span class="tt-label-new">{{ trans('general.new') }}</span>--}}
 {{--    @endif--}}
@@ -16,17 +16,20 @@
 {{--    @if($element->isOnSale)--}}
 {{--        <span class="tt-label-sale">{{ trans('general.on_sale') }}</span>--}}
 {{--    @endif--}}
-    @if($element->is_available && $element->hasStock)
-        <span class="tt-label-new"
-              style="background-color: #978d2f;">{{ trans('general.available_for_booking') }}</span>
-    @else
-        <span class="tt-label-new"
-              style="background-color: #972500;">{{ trans('general.out_of_stock') }}</span>
-    @endif
+{{--    @if($element->is_available)--}}
+{{--        <span class="tt-label-new"--}}
+{{--              style="background-color: #978d2f;">{{ trans('general.available_for_booking') }}</span>--}}
+{{--    @else--}}
+{{--        <span class="tt-label-new"--}}
+{{--              style="background-color: #972500;">{{ trans('general.not_available_for_booking') }}</span>--}}
+{{--    @endif--}}
 {{--    @if($element->is_featured)--}}
 {{--        <span class="tt-label-our-fatured">{{ trans('general.featured') }}</span>--}}
 {{--    @endif--}}
 {{--    @if($element->user)--}}
 {{--        <span class="tt-label-new">{{ $element->user->country->slug }}</span>--}}
 {{--    @endif--}}
-</span>
+{{--</span>--}}
+@if($element->hasStock)
+    <span class="tt-label-new">{{ trans('general.out_of_stock') }}</span>
+@endif
