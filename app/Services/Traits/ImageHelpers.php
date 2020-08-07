4f7963084c09777bbb06c3547841fc975609e232
uusa35 <uusa35@gmail.com>
2020-08-08 02:36:54 +0300

    updates

--- app/Services/Traits/ImageHelpers.php
+++ app/Services/Traits/ImageHelpers.php
@@ -189,7 +189,7 @@ trait ImageHelpers
                                 $imagePath = str_replace('public/uploads/images/', '', $path);
                                 foreach ($sizes as $k => $value) {
 //                                    $image->storePublicly('public/uploads/images/' . $value . '', 'do');
-                                    Storage::copy($path,'public/uploads/images/'.$value.'/'.$imagePath,'public');
+                                    Storage::disk('do')->copy($path,'public/uploads/images/'.$value.'/'.$imagePath,'public');
                                 }
                             } catch (Exception $e) {
                                 return $e->getMessage();
@@ -207,7 +207,8 @@ trait ImageHelpers
                             $path = $request[$inputName][0]->storePublicly('public/uploads/images/', 'do');
                             $imagePath = str_replace('public/uploads/images/', '', $path);
                             foreach ($sizes as $k => $value) {
-                                $request[$inputName][0]->storePublicly('public/uploads/images/' . $value . '', 'do');
+//                                $request[$inputName][0]->storePublicly('public/uploads/images/' . $value . '', 'do');
+                                Storage::disk('do')->copy($path,'public/uploads/images/'.$value.'/'.$imagePath,'public');
                             }
                         } catch (Exception $e) {
                             return $e->getMessage();
