1e315c58ca962bb4c01d62f9efb3c7d827ec68e1
uusa35 <uusa35@gmail.com>
2020-08-18 20:42:15 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_pages_main_menu.blade.php
+++ resources/views/frontend/wokiee/four/partials/_pages_main_menu.blade.php
@@ -1,5 +1,5 @@
 @if(isset($pages) && $pages->isNotEmpty())
-    @foreach($pages->where('on_menu_desktop',true)->take(3) as $page)
+    @foreach($pages->where('on_menu_desktop',true)->take(4) as $page)
         <li class="dropdown">
             <a href="{{ $page->url ? $page->url : route('frontend.page.show.name',['id' => $page->id,'name' => $page->title]) }}">
                 {{ str_limit($page->title,20,'') }}
