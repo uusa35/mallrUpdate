94cfe8ab957252f38b9f4bd99d1dc6778561f538
uusa35 <uusa35@gmail.com>
2020-08-19 08:48:37 +0300

    updates

--- app/Resources/ProductResource.php
+++ app/Resources/ProductResource.php
@@ -33,7 +33,7 @@ class ProductResource extends JsonResource
             'convertedFinalPrice' => $this->convertedFinalPrice,
             'weight' => $this->weight,
             'sale_price' => (double)$this->sale_price,
-            'size_chart_image' => $this->getCurrentImageAttribute('size_chart_image','large'),
+            'size_chart_image' => $this->size_chart_image,
             'qr' => $this->getCurrentImageAttribute('qr', 'large'),
             'description' => $this->description,
             'notes' => $this->notes,
