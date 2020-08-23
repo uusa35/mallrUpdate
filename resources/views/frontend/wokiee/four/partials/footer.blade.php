7044dd0097c239c5422303e0c0f712aeb1c40e66
uusa35 <uusa35@gmail.com>
2020-08-23 13:50:03 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/footer.blade.php
+++ resources/views/frontend/wokiee/four/partials/footer.blade.php
@@ -103,7 +103,7 @@
                         </div>
                     </div>
                 </div>
-                @if(count(trans('message.footer_about_us')) > 5)
+                @if(strlen(trans('message.footer_about_us')) > 5)
                     <div class="col-md-6 col-lg-4 col-xl-3">
                         <div class="tt-mobile-collapse">
                             <h4 class="tt-collapse-title">
