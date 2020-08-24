1c500f3e3cfce38bc0460b2a950e26f7cf1574d9
uusa35 <uusa35@gmail.com>
2020-08-24 09:44:37 +0300

    updates

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -482,10 +482,10 @@
                             <li class="active">
                                 <a href="#portlet_tab_1" data-toggle="tab"> {{ trans('general.sizes') }} </a>
                             </li>
-                            <li>
-                                <a href="#portlet_tab_2"
-                                   data-toggle="tab"> {{ trans('general.videos_for_new_items_for_super') }}</a>
-                            </li>
+                            {{--                            <li>--}}
+                            {{--                                <a href="#portlet_tab_2"--}}
+                            {{--                                   data-toggle="tab"> {{ trans('general.videos_for_new_items_for_super') }}</a>--}}
+                            {{--                            </li>--}}
                         @endcan
                         <li class="">
                             <a href="#portlet_tab_3" data-toggle="tab">{{ trans('general.videos_for_new_items') }} </a>
@@ -503,16 +503,16 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="tab-pane" id="portlet_tab_2">
-                                <div class="scroller" style="height:100%" data-rail-visible="1"
-                                     data-rail-color="yellow"
-                                     data-handle-color="#a1b2bd">
-                                    @include('backend.partials._videos_for_super')
-                                </div>
-                            </div>
+                            {{--                            <div class="tab-pane" id="portlet_tab_2">--}}
+                            {{--                                <div class="scroller" style="height:100%" data-rail-visible="1"--}}
+                            {{--                                     data-rail-color="yellow"--}}
+                            {{--                                     data-handle-color="#a1b2bd">--}}
+                            {{--                                    @include('backend.partials._videos_for_super')--}}
+                            {{--                                </div>--}}
+                            {{--                            </div>--}}
                         @endif
                         <div class="tab-pane" id="portlet_tab_3">
-                            <div class="scroller" data-rail-visible="1"
+                            <div class="scroller" data-rail-visible="1" style="height:100%"
                                  data-rail-color="yellow" data-handle-color="#a1b2bd">
                                 @include('backend.partials._videos_for_company')
                             </div>
