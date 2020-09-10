d4159c5c55cbf4fff311ed10abb2f2e0dfd3686a
uusa35 <uusa35@gmail.com>
2020-09-10 08:40:09 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_search_form.blade.php
+++ resources/views/frontend/wokiee/four/partials/_search_form.blade.php
@@ -1,4 +1,4 @@
-@if(env('MALLR') || env('DAILY') || env('HTB'))
+@if(env('MALLR') || env('DAILY') || env('HTB') || env('NASH'))
     @include('frontend.wokiee.four.partials._search_menu_products')
 @elseif(env('EVENTKM'))
     @include('frontend.wokiee.four.partials._search_menu_services')
