b14e036cf36b5dfae9ca2cf97a626bab03c852e4
uusa35 <uusa35@gmail.com>
2020-09-10 23:29:27 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/styles.blade.php
+++ resources/views/frontend/wokiee/four/partials/styles.blade.php
@@ -268,7 +268,7 @@
         color: {{ $settings->icon_theme_color }}             !important;
     }
 
-    @if(env('NASHKW'))
+    @if(env('NASHKW') || env('BITS'))
     body, html, div, a, p, h1, h2, h3, h4, h5, h6, table, row, td, th, tr, thead, tfoot, button, span, .btn, .btn-del, ul, li, .page-content, .portlet, .light, .profile-sidebar-portlet {
         font-family: 'GE SS Unique', 'sans-serif' !important;
         text-transform: uppercase !important;
