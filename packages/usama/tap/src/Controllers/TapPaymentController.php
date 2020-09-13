dd783f7f7c31cb29bb784382fbd5dfa5d0408f9d
uusa35 <uusa35@gmail.com>
2020-09-13 08:58:29 +0300

    updates

--- packages/usama/tap/src/Controllers/TapPaymentController.php
+++ packages/usama/tap/src/Controllers/TapPaymentController.php
@@ -53,6 +53,7 @@ class TapPaymentController extends Controller
             $order = $order->whereId($request->order_id)->with('order_metas.product', 'order_metas.product_attribute')->first();
             $user = auth()->user();
             $paymentUrl = $this->processPayment($order, $user);
+            dd($paymentUrl);
             if ($paymentUrl) {
                 return redirect()->to($paymentUrl);
             }
