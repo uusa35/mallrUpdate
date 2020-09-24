c136215161282a43733644c3022c4fd0a0e58420
uusa35 <uusa35@gmail.com>
2020-09-24 11:08:47 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/styles.blade.php
+++ resources/views/frontend/wokiee/four/partials/styles.blade.php
@@ -291,7 +291,7 @@
         height : auto !important;
     }
 
-    .modal .modal-header .close:before, .mfp-close:before, .mfp-arrow-right:before, .mfp-arrow:after, .slider-revolution.revolution-default .tp-rightarrow:before, .slider-revolution.revolution-default .tp-leftarrow:before, body:not(.touch-device) .tt-portfolio-content.layout-default figure figcaption .tt-btn-zomm::before {
+    .modal .modal-header .close:before, .mfp-close:before, .mfp-arrow-right:before, .mfp-arrow:after, .slider-revolution.revolution-default .tp-rightarrow:before, .slider-revolution.revolution-default .tp-leftarrow:before, body:not(.touch-device) .tt-portfolio-content.layout-default figure figcaption .tt-btn-zomm::before, .mfp-arrow:after {
         font-family: wokiee !important;
     }
 
