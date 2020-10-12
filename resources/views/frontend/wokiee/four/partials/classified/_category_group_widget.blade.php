d1f40addfd7bb2148c8bae73848ea482c6203857
uusa35 <uusa35@gmail.com>
2020-10-12 11:04:04 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/classified/_category_group_widget.blade.php
+++ resources/views/frontend/wokiee/four/partials/classified/_category_group_widget.blade.php
@@ -1,9 +1,9 @@
 @if($element->items->isNotEmpty())
     <div class="col-lg-12">
-        <div style="display: flex; flex : 1; justify-content: center; align-items: center; flex-wrap: wrap; margin : 0.5rem">
+        <div style="display: flex; flex : 1; justify-content: center; align-items: center; flex-wrap: wrap;">
             @foreach($element->items as $item)
                 <div class="text-center"
-                     style="flex: 1; padding : 0.5rem; max-width: 100px; border: 0.5px solid lightgrey; border-radius: 10px; justify-content: center; align-items: center; flex-direction: column; flex-wrap: nowrap">
+                     style="flex: 1; padding : 0.5rem; margin:0.2rem; max-width: 100px; border: 0.3px solid lightgrey; border-radius: 5px; justify-content: center; align-items: center; flex-direction: column; flex-wrap: nowrap">
                     <div>
                         <img class="text-center" style="width: 75px; max-width: 75px;"
                              src="{{ $item->categoryGroup->getImageThumbLinkAttribute() }}"
