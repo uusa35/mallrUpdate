cd70313ca92ed66e05bb9adf29cdff1e4755e522
uusa35 <uusa35@gmail.com>
2020-10-12 11:04:41 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/classified/_category_group_widget.blade.php
+++ resources/views/frontend/wokiee/four/partials/classified/_category_group_widget.blade.php
@@ -1,6 +1,6 @@
 @if($element->items->isNotEmpty())
     <div class="col-lg-12">
-        <div style="display: flex; flex : 1; justify-content: center; align-items: center; flex-wrap: wrap;">
+        <div style="display: flex; flex : 1; justify-content: center; align-items: center; flex-wrap: wrap; margin-top: 1rem; margin-bottom: 1rem;">
             @foreach($element->items as $item)
                 <div class="text-center"
                      style="flex: 1; padding : 0.5rem; margin:0.2rem; max-width: 100px; border: 0.3px solid lightgrey; border-radius: 5px; justify-content: center; align-items: center; flex-direction: column; flex-wrap: nowrap">
