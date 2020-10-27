60fa7d3bf64a7811662d941c54ad44c12fc0c341
uusa35 <uusa35@gmail.com>
2020-10-27 14:45:53 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_show_page_item_title_description_and_prices.blade.php
+++ resources/views/frontend/wokiee/four/partials/_show_page_item_title_description_and_prices.blade.php
@@ -1,19 +1,24 @@
 <div class="tt-wrapper">
-        <div class="tt-title text-center">
-                {{ $element->name }}
-        </div>
-        <div class="tt-price">
-                @if($element->isOnSale)
-                        <span class="new-price">{{ $element->convertedSalePrice}}
-                                {{ $currency->symbol }}</span>
-                        {{--        <span class="old-price">{{ $element->convertedPrice }}--}}
-                        {{--            {{ $currency->symbol }}</span>--}}
-                @else
-                        <span class="new-price">{{ $element->convertedPrice }}
-                                {{ $currency->symbol }}</span>
-                @endif
-        </div>
+    <div class="tt-title text-center">
+        {{ $element->name }}
+    </div>
+    <div class="tt-price">
+        @if($element->isOnSale)
+            <span class="new-price">{{ $element->convertedSalePrice}}
+                {{ $currency->symbol }}</span>
+            {{--        <span class="old-price">{{ $element->convertedPrice }}--}}
+            {{--            {{ $currency->symbol }}</span>--}}
+        @else
+            <span class="new-price">{{ $element->convertedPrice }}
+                {{ $currency->symbol }}</span>
+        @endif
+    </div>
 </div>
 <div class="tt-wrapper">
     {{ $element->description }}
 </div>
+@if($element->barcode)
+        <div class="col-lg-5 col-xs-12" style="margin-left: auto; margin-right: auto;">
+            {!!DNS2D::getBarcodeHTML($element->barcode, env('BARCODE_TYPE'),2,1)!!}
+    </div>
+@endif
