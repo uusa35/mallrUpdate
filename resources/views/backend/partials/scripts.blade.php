d352c964cef08d402b94fb107c92995d4e79a345
uusa35 <uusa35@gmail.com>
2020-10-28 10:53:03 +0300

    updates

--- resources/views/backend/partials/scripts.blade.php
+++ resources/views/backend/partials/scripts.blade.php
@@ -15,7 +15,7 @@
         "onclick": null,
         "showDuration": "1000",
         "hideDuration": "10000",
-        "timeOut": "10000",
+        "timeOut": "15000",
         "extendedTimeOut": "1000",
         "showEasing": "swing",
         "hideEasing": "linear",
