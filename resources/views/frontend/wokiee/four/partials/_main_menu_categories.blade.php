2ded6020f465c1abb591bc321e530adc1a991e18
uusa35 <uusa35@gmail.com>
2020-10-28 13:23:15 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_main_menu_categories.blade.php
+++ resources/views/frontend/wokiee/four/partials/_main_menu_categories.blade.php
@@ -14,7 +14,7 @@
                                         <img src="{{ $cat->getImageThumbLinkAttribute() }}" alt="{{ $cat->name }}"
                                              class="img-responsive img-xs"/>
                                     @else
-                                        <i class="{{ app()->getLocale() === 'ar' ? "icon-e-15" : "icon-e-14" }}"></i>
+                                        <i class="{{ app()->getLocale() === 'ar' ? "icon-e-14" : "icon-e-15" }}"></i>
                                     @endif
                                     <span>{{ str_limit($cat->name,15,'') }}</span>
                                 </a>
