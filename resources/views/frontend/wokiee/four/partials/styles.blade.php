89a2b513f64cf57f5e0517e5fa0b2c223088e733
uusa35 <uusa35@gmail.com>
2020-10-28 12:31:43 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/styles.blade.php
+++ resources/views/frontend/wokiee/four/partials/styles.blade.php
@@ -1,20 +1,20 @@
 @include('frontend.wokiee.partials.styles')
 <style type="text/css">
-    .tt-desctop-menu nav>ul>li.dropdown>a {
+    .tt-desctop-menu nav > ul > li.dropdown > a {
         font-size: small !important;
     }
 
     .tt-logo img {
-        margin-top : 5px;
+        margin-top: 5px;
         /*margin-bottom: 0px;*/
         /*max-height: 60px !important;*/
     }
 
     .tt-mobile-header .tt-logo-container {
-        height : auto !important;
+        height: auto !important;
     }
 
-    .modal .modal-header .close:before, .mfp-close:before, .mfp-arrow-right:before, .mfp-arrow:after, .slider-revolution.revolution-default .tp-rightarrow:before, .slider-revolution.revolution-default .tp-leftarrow:before, body:not(.touch-device) .tt-portfolio-content.layout-default figure figcaption .tt-btn-zomm::before, .mfp-arrow:after, .mfp-arrow:before  {
+    .modal .modal-header .close:before, .mfp-close:before, .mfp-arrow-right:before, .mfp-arrow:after, .slider-revolution.revolution-default .tp-rightarrow:before, .slider-revolution.revolution-default .tp-leftarrow:before, body:not(.touch-device) .tt-portfolio-content.layout-default figure figcaption .tt-btn-zomm::before, .mfp-arrow:after, .mfp-arrow:before {
         font-family: wokiee !important;
     }
 
@@ -26,12 +26,14 @@
         max-height: 720px !important;
     }
 
-    .tt-promo-02>img {
+    .tt-promo-02 > img {
         transform: none !important;
     }
+
     .slick-slide {
-        height : auto !important;
+        height: auto !important;
     }
+
     @if(env('MALLR') || env('ABATI'))
 
     body, html, a, p, h1, h2, h3, h4, h5, h6, table, row, td, th, tr, td, thead, tfoot, span, .btn, ul, li,
@@ -44,10 +46,11 @@
     .form-default .form-group, label, label:not(.error), .tt-cart-total-title, .tt-cart-total-price, .tt-title-large, .tt-title {
         /*font-family: 'fb', 'sans-serif' !important;*/
         @if(app()->isLocale('ar'))
-     font-family: 'skynews', 'sans-serif' !important;
+      font-family: 'skynews', 'sans-serif' !important;
         @else
-     font-family: 'Tajawal-Medium', 'sans-serif' !important;
+      font-family: 'Tajawal-Medium', 'sans-serif' !important;
     @endif
+
 
 
 
@@ -64,10 +67,11 @@
     .tt-collapse-content, .tt-title-options, .form-group, .form, .tt-table-03, select, option, .form-default select.form-control:not([size]):not([multiple])
     .form-default .form-group, label, label:not(.error), .tt-cart-total-title, .tt-cart-total-price, .tt-title-large, .tt-title {
         @if(app()->isLocale('ar'))
-     font-family: 'GE SS Unique', 'sans-serif' !important;
+      font-family: 'GE SS Unique', 'sans-serif' !important;
         @else
-     font-family: 'Tajawal-Medium', 'sans-serif' !important;
+      font-family: 'Tajawal-Medium', 'sans-serif' !important;
     @endif
+
     }
 
     @elseif(env('ESCRAP') || env('HOMEKEY') || env('EXPO') || env('HUDA'))
@@ -79,24 +83,21 @@
     .tt-collapse-content, .tt-title-options, .form-group, .form, .tt-table-03, select, option, .form-default select.form-control:not([size]):not([multiple])
     .form-default .form-group, label, label:not(.error), .tt-cart-total-title, .tt-cart-total-price, .tt-title-large, .tt-title {
         @if(app()->isLocale('ar'))
-     font-family: 'Tajawal-Medium', 'sans-serif' !important;
+      font-family: 'Tajawal-Medium', 'sans-serif' !important;
         @else
-     font-family: 'Tajawal-Medium', 'sans-serif' !important;
+      font-family: 'Tajawal-Medium', 'sans-serif' !important;
     @endif
-
-
-
-
     }
 
     @endif
+
     body, .tt-show, .page-link, .page-item.active, .page-link, .page-item.disabled {
         color: {{ $settings->main_theme_color ? $settings->main_theme_color : 'white' }};
         background-color: {{ $settings->main_theme_bg_color ? $settings->main_theme_bg_color : 'white' }};
     }
 
-    p, .tt-collapse-content p, .tt-shopcart-table01 td, .tt-shopcart-table01 th,  {
-        color: {{ $settings->main_theme_color ? $settings->main_theme_color : 'white' }}               !important;
+    p, .tt-collapse-content p, .tt-shopcart-table01 td, .tt-shopcart-table01 th, {
+        color: {{ $settings->main_theme_color ? $settings->main_theme_color : 'white' }}                !important;
     }
 
     header, .tt-color-scheme-01, .tt-footer-center, footer .tt-footer-custom:last-child
@@ -104,17 +105,17 @@
     .tt-badge-cart,
     .tt-search-type2-rtl, .tt-background, .dot,
     .headerunderline, .stuck.tt-stuck-nav {
-        background-color: {{ $settings->header_theme_bg }}                  !important;
+        background-color: {{ $settings->header_theme_bg }}                   !important;
     }
 
     footer .tt-color-scheme-02, footer, .tt-footer-center, footer .tt-footer-custom:last-child {
-        color: {{ $settings->footer_theme_color }}                  !important;
-        background-color: {{ $settings->footer_bg_theme_color }}                  !important;
+        color: {{ $settings->footer_theme_color }}                   !important;
+        background-color: {{ $settings->footer_bg_theme_color }}                   !important;
     }
 
 
     footer .tt-color-scheme-01 .tt-collapse-content a, footer .tt-color-scheme-01 {
-        color: {{ $settings->footer_theme_color }}                  !important;
+        color: {{ $settings->footer_theme_color }}                   !important;
     }
 
     .btn, .tt-btn-search-rtl, .tt-btn-search,
@@ -129,8 +130,8 @@
     ul.tt-options-swatch li.active a:not(.options-color):not(.options-color-img), ul.tt-options-swatch li:hover a:not(.options-color):not(.options-color-img),
     .btn > .fa .fa-fw,
     .tt-product:not(.tt-view):hover .tt-image-box .tt-btn-quickview, .tt-product:not(.tt-view):hover .tt-image-box .tt-btn-quickview:hover {
-        color: {{ $settings->btn_text_theme_color }}                  !important;
-        background-color: {{ $settings->btn_bg_theme_color }}                  !important;
+        color: {{ $settings->btn_text_theme_color }}                   !important;
+        background-color: {{ $settings->btn_bg_theme_color }}                   !important;
     }
 
     .btn.btn-border {
@@ -141,39 +142,39 @@
     .tt-desctop-menu:not(.tt-hover-02) li.dropdown > a,
     .tt-desctop-menu:not(.tt-hover-02) li.dropdown.active > a,
     .tt-desctop-menu:not(.tt-hover-02), .tt-btn-quickview {
-        color: {{ $settings->main_theme_color }}          !important;
+        color: {{ $settings->main_theme_color }}           !important;
     }
 
     .tt-mobile-header .tt-top-line {
-        border-top: 1px solid {{ $settings->btn_bg_theme_color }}                !important;
+        border-top: 1px solid {{ $settings->btn_bg_theme_color }}                 !important;
     }
 
     li.dropdown.selected > a,
     .tt-menu-toggle.stylization-02,
     header .tt-dropdown-obj .tt-dropdown-toggle,
     header .tt-multi-obj ul li.active a [class^=icon-],
-    .datepicker-panel > ul > li, .tt-submenu a,
+    .datepicker-panel > ul > li, .tt-submenu > a,
     .tt-social-icon li a, .tt-social-icon li a:hover,
     .tt-desctop-menu:not(.tt-hover-02) li.dropdown > a, header .tt-dropdown-obj .tt-dropdown-toggle, .tt-dropdown-toggle {
-        color: {{ $settings->menu_theme_color }}                !important;
+        color: {{ $settings->menu_theme_color }}                 !important;
     }
 
     .tt-desktop-header, .headerunderline {
-        border-bottom-color: {{ $settings->menu_theme_bg }}   !important;
+        border-bottom-color: {{ $settings->menu_theme_bg }}    !important;
         border-bottom-width: 5px;
     }
 
     footer .tt-color-scheme-02 {
-        border-top: 5px solid {{ $settings->menu_theme_bg }}  !important;
+        border-top: 5px solid {{ $settings->menu_theme_bg }}   !important;
     }
 
     header .tt-color-scheme-01 {
-        background-color: {{ $settings->menu_theme_bg }}    !important;
-        color: {{ $settings->menu_theme_color }}                !important;
+        background-color: {{ $settings->menu_theme_bg }}     !important;
+        color: {{ $settings->menu_theme_color }}                 !important;
     }
 
     header .tt-color-scheme-01 .tt-box-info ul li a, header .tt-social-icon li a {
-        color: {{ $settings->menu_theme_bg ? 'white' : $settings->menu_theme_color }}                !important;
+        color: {{ $settings->menu_theme_bg ? 'white' : $settings->menu_theme_color }}                 !important;
     }
 
     .tt-desctop-menu img {
@@ -198,31 +199,31 @@
     .tt-desctop-menu:not(.tt-hover-02) li.dropdown > a:hover, .tt-block-title,
     .tt-promo-box.hover-type-2:hover, .tt-product:not(.tt-view):hover, .tt-collapse-content a {
 
-        color: {{ $settings->normal_text_theme_color }}                  !important;
+        color: {{ $settings->normal_text_theme_color }}                   !important;
     }
 
     h1, h2, h3, .tt-dropdown-toggle, .countdown-selection .theme-color, .tt-countdown, .tt-countdown_box, .tt-product-single-info > a, .tt-price, .old-price,
     .form-check-label, .card-header, label, label:not(.error), .card-text,
     .money, header .tt-multi-obj ul li.active a, .product-information-buttons a, .tt-services-block .tt-col-icon,
     .tt-title, .tt-collapse-title, .tt-block-title, .tt-block-title .tt-title a {
-        color: {{ $settings->header_one_theme_color }}            !important;
+        color: {{ $settings->header_one_theme_color }}             !important;
     }
 
     .tt-product:not(.tt-view) .tt-description .tt-title a, .tt-shopcart-table-02 .tt-title a,
     .tt-table-shop-01 tbody td a {
-        color: {{ $settings->header_tow_theme_color }}            !important;
+        color: {{ $settings->header_tow_theme_color }}             !important;
     }
 
     @media (max-width: 1024px) {
         .tt-mobile-header {
-            background-color: {{ $settings->header_theme_bg }}                  !important;
+            background-color: {{ $settings->header_theme_bg }}                   !important;
             padding-bottom: 10px;
         }
     }
 
     a .url-normal {
         text-decoration: none !important;
-        color: {{ $settings->main_text_theme_color }}                  !important;
+        color: {{ $settings->main_text_theme_color }}                   !important;
         text-underline: none !important;
     }
 
@@ -240,11 +241,11 @@
     }
 
     .tt-col-icon, .tt-services-block .tt-col-icon {
-        color: {{ $settings->icon_theme_color }}             !important;
+        color: {{ $settings->icon_theme_color }}              !important;
     }
 
-    .tt-menu-categories .tt-dropdown-menu nav>ul>li.tt-submenu>a:after {
-        display: none!important;
+    .tt-menu-categories .tt-dropdown-menu nav > ul > li.tt-submenu > a:after {
+        display: none !important;
     }
 
     @if(env('NASHKW') || env('BITS') || ENV('EMAKEUP'))
@@ -265,7 +266,7 @@
                 /*text-transform: uppercase !important;*/
             }
 
-            .tt-desctop-menu nav>ul>li.dropdown>a {
+            .tt-desctop-menu nav > ul > li.dropdown > a {
                 font-size: large !important;
             }
 
@@ -295,7 +296,7 @@
                 text-transform: uppercase !important;
             }
 
-            .tt-desctop-menu nav>ul>li.dropdown>a {
+            .tt-desctop-menu nav > ul > li.dropdown > a {
                 font-size: large !important;
             }
 
