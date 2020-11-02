47138ea67ab480bee6c64d7c5a84a6889d4cfc5c
uusa35 <uusa35@gmail.com>
2020-11-02 10:24:19 +0300

    updates

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="page-sidebar-wrapper">
     <div class="page-sidebar navbar-collapse collapse">
-{{--        <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu page-sidebar-menu-closed"--}}
-{{--            data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">--}}
-        <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200" style="padding-top: 20px">
-            <li class="sidebar-toggler-wrapper hide">
+        <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu page-sidebar-menu-closed"
+            data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+{{--        <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200" style="padding-top: 20px">--}}
+{{--            <li class="sidebar-toggler-wrapper hide">--}}
                 <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
                 <div class="sidebar-toggler">
                     <span></span>
