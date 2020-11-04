194f9635dedafa7117331542220752969bf87aa3
uusa35 <uusa35@gmail.com>
2020-11-04 18:06:27 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_classified_widget.blade.php
+++ resources/views/frontend/wokiee/four/partials/_classified_widget.blade.php
@@ -8,9 +8,6 @@
                data-tposition="{{ app()->isLocale('ar') ? 'right' : 'left' }}"
             ></a>
         @endauth
-        {{--<a href="#" class="tt-btn-compare" data-tooltip="Add to Compare"--}}
-        {{--data-tposition="{{ app()->isLocale('ar') ? 'right' : 'left' }}"--}}
-        {{--></a>--}}
         <a href="{{ route('frontend.classified.show.name', ['id' => $element->id, 'name' => $element->name]) }}">
             @include('frontend.wokiee.four.partials._widget_tags_and_images')
         </a>
@@ -18,5 +15,26 @@
             @include('frontend.wokiee.four.partials._widget_is_really_hot')
         @endif
     </div>
-    @include('frontend.wokiee.four.partials._product_widget_description')
+    <div class="tt-description text-center">
+        <div class="tt-row">
+            <ul class="tt-add-info">
+                <li>
+                    <a href="{{ route('frontend.classified.search',['user_id' => $element->user_id]) }}">{{ str_limit($element->user->slug,25,'..') }}</a>
+                </li>
+        </div>
+        <h2 class="tt-title">
+            <a href="{{ route('frontend.classified.show.name', ['id' => $element->id, 'name' => $element->name]) }}">{{ str_limit($element->name,25,'..') }}</a>
+        </h2>
+        @include('frontend.wokiee.four.partials._widget_price_and_color')
+        <div class="tt-product-inside-hover text-center">
+            <div class="tt-row-btn">
+                <a href="{{ route('frontend.classified.show.name', ['id' => $element->id, 'name' => $element->name]) }}"
+                   class="btn btn-small mb-2">
+                    <i class="icon-g-89"></i>
+                    {{ trans('general.view_details') }}
+                </a>
+            </div>
+        </div>
+    </div>
+
 </div>
