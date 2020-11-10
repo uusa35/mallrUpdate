8d2c2e764e00c6d28e87c7692ab9c76f4da7d8e4
uusa35 <uusa35@gmail.com>
2020-11-10 21:14:28 +0300

    updates

--- app/Resources/ProductExtraLightResource.php
+++ app/Resources/ProductExtraLightResource.php
@@ -27,6 +27,7 @@ class ProductExtraLightResource extends JsonResource
             'thumb' => $this->getCurrentImageAttribute(),
             'isReallyHot' => $this->isReallyHot,
             'hasStock' => $this->hasStock,
+            'country_id' => $this->user->country_id,
         ];
     }
 }
